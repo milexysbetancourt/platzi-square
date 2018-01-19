ba4e38a766f1e20ed32b299d3878ba58da400e87
Milexys <milexysbetancourt1@gmail.com>
2018-01-18 20:54:37 -0400

    Add google-maps library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mi proyecto en Angular 4';
+  lat:number = 10.1520342;
+  lng:number = -68.4004204;
+  constructor(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD2xXZKtt-xry9QiguynnxrpuDfevpyX7U'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
