bd73ad72dd58a31ff856c0ac4f63807d2909bde1
Milexys <milexysbetancourt1@gmail.com>
2018-01-22 11:54:25 -0400

    Ejercicios con directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mi proyecto en Angular 4';
+  lugares:any = [
+    {plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre: 'Heladeria 4D'},
+    {plan: 'gratuito', cercania: 1, distancia: 6.2, active: false, nombre: 'Presto Pizza'},
+    {plan: 'pagado', cercania: 2, distancia: 4, active: true, nombre: 'La Mansión del Pan'},
+    {plan: 'gratuito', cercania: 2, distancia: 15, active: true, nombre: 'Pizzeria La Mejor Pizza'},
+    {plan: 'pagado', cercania: 3, distancia: 38, active: false, nombre: 'El mejor negocio de comida'},
+    {plan: 'gratuito', cercania: 3, distancia: 45, active: true, nombre: 'A donde tú quieras'}
+  ];
   lat:number = 10.1520342;
   lng:number = -68.4004204;
   constructor(){
